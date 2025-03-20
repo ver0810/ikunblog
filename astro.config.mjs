fc8b97c4e22f28da1a54088a588813a931c7aabf
ancheng <ver_0810@163.com>
2025-03-20 21:17:27 +0800

    2025-3-20

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,31 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import icon from 'astro-icon';
-import remarkMath from 'remark-math'
-import rehypeMathjax from 'rehype-mathjax'
+import icon from "astro-icon";
+import remarkMath from "remark-math";
+import rehypeMathjax from "rehype-mathjax";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
+  output: 'static',
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathjax],
     shikiConfig: {
       theme: "catppuccin-frappe",
-    }
+    },
   },
-  integrations: [icon({
-    include: {
-      'fa-solid': ['*'],
-      'lucide': ['*'],
-    }
-  })],
+  integrations: [
+    icon({
+      include: {
+        "fa-solid": ["*"],
+        lucide: ["*"],
+      },
+    }),
+  ],
   vite: {
-    plugins: [tailwindcss(),],
+    plugins: [tailwindcss()],
   },
 });
