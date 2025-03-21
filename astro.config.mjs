76000957cf8958b38a8dc959752409f68265a20e
ver0810 <53964268+ver0810@users.noreply.github.com>
2025-03-21 23:05:10 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,7 @@ import remarkCallout from "remark-callout";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ver0810.github.io/',
-  base: '/verblog',
+  site: 'https://ver0810.github.io/verblog/',
   markdown: {
     remarkPlugins: [ [remarkCallout, { types: ['note', 'warning', 'tip', 'error'] }], [remarkMath, remarkObsidianCallout]],
     rehypePlugins: [rehypeMathjax],
